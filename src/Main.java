0f8d0f3078f29e584fe8d2be84b6e441fee3e6a2
MJ-ZOU <593926020@qq.com>
2019-04-11 20:42:32 +0800

    hs

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        String s = sc.nextLine();
+        int[] ints = toIntArr(s);
+
+        for (int i : ints) {
+            int k = i;
+            int sum = 0;
+            boolean b = true;
+            Set<Integer> set = new HashSet<>();
+            set.add(k);
+            while (k != 1) {
+                if (k % 2 == 0) {
+                    k = k / 2;
+                } else {
+                    k = 3 * k + 1;
+                }
+                b = set.add(k);
+                sum++;
+                if (!b) {
+                    System.out.println(-1);
+                    break;
+                }
+            }
+            if (!b){
+                continue;
+            }
+            System.out.println(sum);
+        }
+
+
+    }
+
+
+    //带空格的字符串转数组
+    static int[] toIntArr(String str) {
+        String[] strings = str.split(" ");
+        int[] arr = new int[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            arr[i] = new Integer(strings[i]);
+        }
+        return arr;
+    }
+
+
+}
