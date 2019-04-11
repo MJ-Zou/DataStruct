8f4686c8378df2a3b7c333d637a63c429cd21f1b
MJ-Zou <593926020@qq.com>
2019-04-11 21:44:25 +0800

    sort

---
+++ src/WeBank/Main.java
@@ -0,0 +1,52 @@
+package WeBank;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+        String s = sc.nextLine();
+        int[] ints = toIntArr(s);
+
+        for (int i : ints) {
+            int k = i;
+            int sum = 0;
+            boolean b = true;
+            Set<Integer> set = new HashSet<>();
+            set.add(k);
+            while (k != 1) {
+                if (k % 2 == 0) {
+                    k = k / 2;
+                } else {
+                    k = 3 * k + 1;
+                }
+                b = set.add(k);
+                sum++;
+                if (!b) {
+                    System.out.println(-1);
+                    break;
+                }
+            }
+            if (!b){
+                continue;
+            }
+            System.out.println(sum);
+        }
+
+
+    }
+
+
+    //带空格的字符串转数组
+    private static int[] toIntArr(String str) {
+        String[] strings = str.split(" ");
+        int[] arr = new int[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            arr[i] = new Integer(strings[i]);
+        }
+        return arr;
+    }
+
+
+}
---
+++ src/WeBank/Main1.java
@@ -0,0 +1,59 @@
+package WeBank;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int[] ints = toIntArr(s);
+        long n = ints[0];
+        int m = ints[1];
+
+        if (n == 1) {
+            System.out.println(" ");
+            System.out.println(1);
+            return;
+        }
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        int loc = 0;
+        while (list.size() > 1) {
+            if (loc >= list.size()) {
+                loc = 0;
+            }
+            for (int i = 0; i < m - 1; i++) {
+                loc = loc + 1;
+                if (loc >= list.size()) {
+                    loc = 0;
+                }
+            }
+            if (list.size() == 2) {
+                System.out.print(list.remove(loc) + 1);
+            } else {
+                System.out.print(list.remove(loc) + 1 + " ");
+            }
+
+        }
+        System.out.println();
+        System.out.println(list.getFirst() + 1);
+
+
+    }
+
+
+    //带空格的字符串转数组
+    private static int[] toIntArr(String str) {
+        String[] strings = str.split(" ");
+        int[] arr = new int[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            arr[i] = new Integer(strings[i]);
+        }
+        return arr;
+    }
+
+
+}
