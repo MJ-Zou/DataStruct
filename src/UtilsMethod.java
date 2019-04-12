6b101d6a14e1531ea2cfb1480b9145183c882225
MJ-Zou <593926020@qq.com>
2019-04-12 09:24:51 +0800

    sort

--- src/UtilsMethod.java
+++ src/UtilsMethod.java
@@ -1,21 +1,19 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 
 //工具方法11
 public class UtilsMethod {
 
     public static void main(String[] args) {
-        int[] ints = toIntArr("1 2 3 4");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int[] ints = toIntArr(s);// s="1 2 3 4"
 
         List<int[]> list = new ArrayList<>();
         allSort(ints, 0, ints.length - 1, list);
 
-        String[] chs = {"1", "2", "3"};
-        List<LinkedList<String>> linkedLists = new ArrayList<>();
-        combine(chs, linkedLists);
+        List<LinkedList<Integer>> list1 = new ArrayList<>();
+        combine(ints, list1);
     }
 
     //带空格的字符串转数组
@@ -49,17 +47,17 @@ public class UtilsMethod {
     }
 
     //全组合
-    static void combine(String chs[], List<LinkedList<String>> comb) {
+    private static void combine(int[] chs, List<LinkedList<Integer>> comb) {
         if (chs.length == 0) return;
-        LinkedList<String> list = new LinkedList<>();
+        LinkedList<Integer> list = new LinkedList<>();
         for (int i = 1; i <= chs.length; i++) {
             combine(chs, 0, i, list, comb);
         }
     }
 
-    private static void combine(String[] chs, int begin, int number, LinkedList<String> list, List<LinkedList<String>> comb) {
+    private static void combine(int[] chs, int begin, int number, LinkedList<Integer> list, List<LinkedList<Integer>> comb) {
         if (number == 0) {
-            LinkedList<String> l = new LinkedList<>(list);
+            LinkedList<Integer> l = new LinkedList<>(list);
             comb.add(l);
             return;
         }
