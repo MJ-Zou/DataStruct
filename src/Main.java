6118fa2633043857b6163c800530fad67dcb0d74
MJ-Zou <593926020@qq.com>
2019-04-14 16:33:45 +0800

    sort

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,9 @@
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(GetUglyNumber_Solution(100));
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
     }
-
-
-    private static int GetUglyNumber_Solution(int index) {
-        int num = 0;
-        int n = 0;
-        while (num < index) {
-            n++;
-            while (!isUgly(n)) {
-                n++;
-            }
-            num++;
-        }
-        return n;
-    }
-
-    private static boolean isUgly(int n) {
-        while (n % 2 == 0) {
-            n = n / 2;
-        }
-        while (n % 3 == 0) {
-            n = n / 3;
-        }
-        while (n % 5 == 0) {
-            n = n / 5;
-        }
-        return n == 1;
-    }
-
 }
